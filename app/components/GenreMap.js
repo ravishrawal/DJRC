6c6392ad42666c0c8512e931b16e5e6c8a9169d8
Ravish Rawal <rr2914@columbia.edu>
2017-11-07 21:39:42 -0500

    polyline twerking

--- app/components/GenreMap.js
+++ app/components/GenreMap.js
@@ -15,8 +15,8 @@ export default class GenreMap extends React.Component {
           longitudeDelta: 0.008
         },
         destLocation: {
-          latitude: 40.72908445,
-          longitude: -73.97863847
+          latitude: 40.72581151,
+          longitude: -73.98489475
         }
       };
       this.onRegionChange = this.onRegionChange.bind(this)
@@ -25,22 +25,24 @@ export default class GenreMap extends React.Component {
     componentDidMount(){
       navigator.geolocation.getCurrentPosition((res, rej)=>{
         res ? this.setState({currentLocation: {latitude: res.coords.latitude, longitude: res.coords.longitude} }) : console.log(rej);
+        console.log(res.coords);
       })
-      this.getDirections().then(()=>console.log('state set'));
+      setTimeout(()=>this.getDirections().then(()=>console.log('state set')),2000);
     }
     onRegionChange(region){
       let { latitude, longitude, latitudeDelta, longitudeDelta } = region;
       this.setState({ currentLocation: { latitude, longitude }, regionSize: { latitudeDelta, longitudeDelta} })
     }
     async getDirections(startLoc=this.state.currentLocation, destinationLoc=this.state.destLocation) {
+      console.log(startLoc, destinationLoc);
         try {
-            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc.latitude },${startLoc.longitude}&destination=${ destinationLoc.latitude },${destinationLoc.longitude}`)
+            let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc.latitude },${startLoc.longitude}&destination=${ destinationLoc.latitude },${destinationLoc.longitude}&mode=walking`)
             let respJson = await resp.json();
             let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
             let coords = points.map((point, index) => {
                 return  {
-                    latitude : point[0],
-                    longitude : point[1]
+                    latitude :point[0],
+                    longitude :point[1]
                 }
             })
             this.setState({coords: coords})
@@ -62,21 +64,20 @@ export default class GenreMap extends React.Component {
                 <MapView
                     style={styles.map}
                     initialRegion={Object.assign({}, currentLocation, regionSize)}
-                    onRegionChange={this.onRegionChange}
                     showsUserLocation={true}>
 
                     <MapView.Marker
                         coordinate={coordinate}
                         title='Airbnb'
                     />
+                  {coords &&
+                    <MapView.Polyline
+                      coordinates={coords}
+                      strokeWidth={4}
+                      lineCap='round'
+                      lineJoin='round'
+                      strokeColor="rgba(255,140,0,0.8)"/>}
                 </MapView>
-              }
-              {coords &&
-                <MapView.Polyline
-                  coordinates={coords}
-                  strokeWidth={2}
-                  fillColor="rgb(0, 200, 0)"
-                  strokeColor="rgb(255,0,0)"/>
               }
                 <View style={styles.search}>
                     <SearchBar
