b940390a024fcd3e7c9055315e4b009d200bafad
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-11-24 17:05:17 -0500

    comit

--- app/redux/bars.js
+++ app/redux/bars.js
@@ -33,12 +33,15 @@ const getSongsFromSpotify = (bar) => {
 }
 
 export const fetchBarsFromServer = () => {
+    console.log('adfs')
     return (dispatch) => {
         axios.get('http://192.168.0.14:3002/api/venues')
             .then(res => res.data)
             .then(bars => {
                 dispatch(getBars(bars));
-            }).catch(console.log);
+            }).catch(err => {
+                console.log('err', err);
+            });
             
 //                 if (!bars) return;
 //                 bars = bars.length && bars.map(bar => {
