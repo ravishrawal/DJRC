a37fb73b90f01c44ef30ca29ae11c18ec30c74c1
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-12-01 19:50:58 -0500

    imports helpers to SignUpOrIn.js

--- app/components/SignUpOrIn.js
+++ app/components/SignUpOrIn.js
@@ -1,79 +1,73 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Linking, TouchableHighlight, Text } from 'react-native'
+import { View, StyleSheet, Linking, TouchableHighlight, Text } from 'react-native';
 import axios from 'axios';
-import { FormLabel, FormInput } from 'react-native-elements'
+import { FormLabel, FormInput } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { WebBrowser} from 'expo';
 
 import { getUser, spotifyLogin, signUp } from '../redux/user';
 
+import colors from '../helper/colors.js';
+import fonts from '../helper/fonts.js';
+import commonStyles from '../helper/styles.js';
+
 class SignUpOrIn extends Component {
     constructor() {
-        super()
+        super();
         this.state = {
             email: '',
             password: '',
             token: ''
-        }
+        };
         this.onChangeEmail = this.onChangeEmail.bind(this);
         this.onChangePassword = this.onChangePassword.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
         this.login = this.login.bind(this);
         this.handleRedirect = this.handleRedirect.bind(this);
     }
-    componentDidMount(){
+    componentDidMount() {
         Linking.addEventListener('url', this.handleRedirect);
     }
-
     onChangeEmail(email) {
-        this.setState({ email: email })
+        this.setState({ email: email });
     }
-
     onChangePassword(password) {
-        this.setState({ password: password })
+        this.setState({ password: password });
     }
-
     handleAdd() {
         const credentials = {
             email: this.state.email,
             password: this.state.password,
-        }
+        };
         this.props.signUp(credentials);
-
     }
-
     login() {
         const { navigate } = this.props.navigation;
         const credentials = {
             email: this.state.email,
             password: this.state.password,
-        }
-        this.props.getUser(credentials, navigate)
+        };
+        this.props.getUser(credentials, navigate);
     }
-
     handleRedirect(event) {
         WebBrowser.dismissBrowser();
         let ev = event.url.split('=');
         const token = ev[1];
-        this.props.spotifyLogin(token)
+        this.props.spotifyLogin(token);
     }
-
-
     spotLogin() {
         Linking.addEventListener('url', this.handleRedirect);
-        WebBrowser.openBrowserAsync(`https://djrc-api.herokuapp.com/passportAuth/spotify`)
+        WebBrowser.openBrowserAsync(`https://djrc-api.herokuapp.com/passportAuth/spotify`);
         Linking.removeEventListener('url', this.handleRedirect);
     }
-
-
     render() {
         const { handleAdd, onChangeEmail, onChangePassword, login } = this;
         return (
             <View style={styles.container}>
                 <FormLabel>Email</FormLabel>
-                <FormInput onChangeText={onChangeEmail} ></FormInput>
+                <FormInput onChangeText={onChangeEmail}></FormInput>
                 <FormLabel>Password</FormLabel>
-                <FormInput name='password' onChangeText={onChangePassword} ></FormInput>
+                <FormInput name="password" onChangeText={onChangePassword}></FormInput>
                 <TouchableHighlight onPress={handleAdd}>
                     <Text style={[styles.button, styles.greenButton]}>Create account</Text>
                 </TouchableHighlight>
@@ -84,7 +78,7 @@ class SignUpOrIn extends Component {
                     <Text style={[styles.button, styles.greenButton]}>Spotify</Text>
                 </TouchableHighlight>
             </View>
-        )
+        );
     }
 }
 
@@ -107,14 +101,14 @@ const styles = StyleSheet.create({
     centering: {
         alignItems: 'center',
         justifyContent: 'center'
-    }
-})
+    },
+});
 
 const mapState = ({ user }) => {
     return {
-        user
-    }
-}
+        user,
+    };
+};
 
 const mapDispatch = (dispatch) => {
     return {
@@ -126,8 +120,8 @@ const mapDispatch = (dispatch) => {
         },
         signUp: (credentials) => {
             dispatch(signUp(credentials));
-        }
-    }
-}
+        },
+    };
+};
 
 export default connect(mapState, mapDispatch)(SignUpOrIn);
