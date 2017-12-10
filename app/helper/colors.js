a120d3d4e90f1c7e39afe27a91caf5c788814165
Colin FitzGerald <colinclarkfitzgerald@gmail.com>
2017-12-10 16:58:57 -0500

    linting and formatting for app/helper

--- app/helper/colors.js
+++ app/helper/colors.js
@@ -1,13 +1,13 @@
 const colors = {
-  blue: '#00c3e3',
-  gray: '#777', // text
-  offWhite: '#f7f7f7', // background
-  pink: '#ff4554',
-  redOrange: '#ff6763', // logo
-  redOrangeDark: '#e5413d',
-  yellow: '#ffcba4',
-  yellowDark: '#ad6938',
-  yellowLight: '#fdfea4',
+    blue: '#00c3e3',
+    gray: '#777', // text
+    offWhite: '#f7f7f7', // background
+    pink: '#ff4554',
+    redOrange: '#ff6763', // logo
+    redOrangeDark: '#e5413d',
+    yellow: '#ffcba4',
+    yellowDark: '#ad6938',
+    yellowLight: '#fdfea4',
 };
 
 export default colors;
